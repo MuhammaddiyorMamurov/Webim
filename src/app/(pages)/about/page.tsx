f8bac8b5a5def21f725f9d6efb9b8bb9dbf4c022
MuhammaddiyorMamurov <muhammaddiyormamurov8908@gmail.com>
2025-11-15 16:02:18 +0500

    main

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="container flex flex-col gap-5">
       <h2 className="text-3xl font-bold">About Us</h2>
-      <p><span className="text-center text-3xl text-green-900">Webim.uz</span> is a leading digital news platform in Uzbekistan, dedicated to delivering timely, accurate, and impartial coverage of the most significant events in Uzbekistan and across the globe.</p>
+      <p><span className="text-center text-3xl text-green-900">Users.uz</span> is a leading digital news platform in Uzbekistan, dedicated to delivering timely, accurate, and impartial coverage of the most significant events in Uzbekistan and across the globe.</p>
     </div>
   )
 }
